e2a9f9a6655c47ca97872628452781097699e975
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2022-10-05 13:05:51 +0700

    add telegram bot

---
+++ cronjob.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/go-co-op/gocron"
+	"log"
+	"net/http"
+	"time"
+)
+
+// main func
+func main() {
+
+	logger := NewLogger()
+
+	s := gocron.NewScheduler(time.UTC)
+	// Reset limit send mail for users.
+	_, err := s.Every(1).Hour().Do(func() {
+		logger.Info("Call url .")
+		// get message from getQuote function
+		message := getQuote()
+		callURL(message)
+	})
+
+	if err != nil {
+		logger.Error("Error scheduling limit data", "error", err)
+		return
+	}
+
+	//s.StartAsync()
+	s.StartBlocking()
+}
+
+// call url
+func callURL(message string) {
+	// get http with message param
+	if message != "" {
+		resp, err := http.Get("https://api.telegram.org/bot5520678085:AA/sendMessage?chat_id=-1001880226194&text=" + message + "&parse_mode=markdown")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+	}
+}
+
+// get a quotes random
+func getQuote() string {
+	resp, err := http.Get("https://zenquotes.io/api/random")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	var quotes []Quote
+
+	if err := json.NewDecoder(resp.Body).Decode(&quotes); err != nil {
+		log.Fatalln(err)
+	}
+	if len(quotes) > 0 {
+		return quotes[0].Q + " - " + quotes[0].A
+	}
+	return ""
+}
+
+// Quote struct
+type Quote struct {
+	Q string `json:"q"`
+	A string `json:"a"`
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module cronJobProject
+
+go 1.18
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-co-op/gocron v1.17.0 // indirect
+	github.com/hashicorp/go-hclog v1.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+)
